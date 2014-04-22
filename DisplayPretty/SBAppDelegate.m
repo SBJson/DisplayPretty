c626641732221a156058f4b22e1b8c3fd4b2ef4b
Stig Brautaset <stig@brautaset.org>
2014-04-22 22:49:17 +0100

    Crudely reflow JSON input

--- DisplayPretty/SBAppDelegate.m
+++ DisplayPretty/SBAppDelegate.m
@@ -6,10 +6,12 @@
 //  Copyright (c) 2014 Stig Brautaset. All rights reserved.
 //
 
+#import <SBJson4.h>
 #import "SBAppDelegate.h"
 
 @interface SBAppDelegate () < NSTextViewDelegate >
 
+@property (strong) SBJson4Writer *writer;
 @property (assign) IBOutlet NSTextView *input;
 @property (assign) IBOutlet NSTextView *output;
 
@@ -19,12 +21,24 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    self.writer = [[SBJson4Writer alloc] init];
+    self.writer.humanReadable = YES;
+    self.writer.sortKeys = YES;
 }
 
 - (void)textDidChange:(NSNotification *)notification
 {
-    self.output.string = self.input.string;
+    id block = ^(id obj, BOOL *ignored) {
+        id data = [self.writer dataWithObject:obj];
+        self.output.string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    };
+    
+    id eh = ^(NSError *err) {
+        self.output.string = err.description;
+    };
+
+    id parser = [SBJson4Parser parserWithBlock:block allowMultiRoot:NO unwrapRootArray:NO errorHandler:eh];
+    [parser parse:[self.input.string dataUsingEncoding:NSUTF8StringEncoding]];
 }
 
 @end
